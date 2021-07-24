8f872cd7fbfcf6b29e0154fac0bf4eb22d015338
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2021-07-24 18:04:45 -0400

    basic landing page

--- frontend/src/Pages/Landing/Landing.js
+++ frontend/src/Pages/Landing/Landing.js
@@ -2,7 +2,28 @@ import React from 'react';
 
 const Landing = () => {
   return(
-    <div>this is the landing page</div>
+
+    <div className="landing">
+
+      <h1 className="welcome">Welcome to spects.io</h1>
+
+      <div className="demo">
+        <p className="demo__placeholder">
+          here is where there will be a video once the site is ready
+        </p>
+      </div>
+
+      <p className="landing__desc">
+        here is some filler text regarding what the site is about and 
+        here is some more information that you can look at to 
+        see if you would be interested in using it. 
+      </p>
+      
+      <button className="btn signup">
+        Sign Up
+      </button>
+      
+    </div>
     
   );
 }
