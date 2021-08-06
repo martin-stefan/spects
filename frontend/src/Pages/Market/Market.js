1fde4dfe36c9b1b15e543835b3f4dc86cfb5b543
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2021-08-05 22:27:19 -0400

    creating basic handler for pop up

--- frontend/src/Pages/Market/Market.js
+++ frontend/src/Pages/Market/Market.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 
 const Market = () => {
+
+  const [viewed, setView] = useState(false);
+
+
+  function handleClickPop() {
+    setView(!viewed);
+  }
+
   return (
     <div className="market">
 
@@ -24,7 +32,7 @@ const Market = () => {
 
           </div>
 
-          <div className="item__img__placeholder">this is a placeholder for an img</div>
+          <img className="item__img__placeholder" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.rxwallpaper.site%2Fwp-content%2Fuploads%2Ffuture-city-wallpapers-wallpaper-cave-1-800x800.jpg&f=1&nofb=1"/>
 
           <div className="item__buttons">
             <button className="seemore">See more</button>
@@ -32,10 +40,32 @@ const Market = () => {
           </div>
         </div>
 
+        <div className="market__item">
+
+
+          <div className="item__header">
+            <p className="item__title">
+              Color theme
+            </p>
+
+            <p className="item__cost">
+              3000
+            </p>
+
+          </div>
+
+          <img className="item__img__placeholder" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.rxwallpaper.site%2Fwp-content%2Fuploads%2Ffuture-city-wallpapers-wallpaper-cave-1-800x800.jpg&f=1&nofb=1"/>
+
+          <div className="item__buttons">
+            <button className="seemore" onClick={() => handleClickPop()}>See more</button>
+            <button className="buy">Buy</button>
+          </div>
+        </div>
+
       </div>
 
 
-      <div className="item__pop">
+      <div className={viewed ? "item__pop": "hidden"}>
 
         <div className="pop__content">
           <div className="pop__header">
